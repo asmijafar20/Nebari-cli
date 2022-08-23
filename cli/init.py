4218f40d81cafe319931472f8003eccdea614d32
eskild <42120229+iameskild@users.noreply.github.com>
2022-08-23 08:33:30 -0700

    Add branching to init

--- cli/init.py
+++ cli/init.py
@@ -7,8 +7,18 @@ def main():
     print("user don't need to set default in init command if you wish to customize something please have a look to qhub --help command and their values")
 
     cloud_provider = typer.prompt("What's your Cloud Provider?")
+    
+    if cloud_provider == "AWS" and not os.getenv('AWS_ACCESS_KEY_ID') and not os.getenv("AWS_SECRET_ACCESS_KEY"):
+        print("Please generate your AWS keys at this link:
+              https://docs.aws.amazon.com/powershell/latest/userguide/pstools-appendix-sign-up.html
+              ")
+        aws_access_key_id = typer.prompt("Please enter your AWS_ACCESS_KEY_ID: ")
+        aws_secret_access_key = typer.prompt("Please enter your AWS_SECRET_ACCESS_KEY: ")
+        
     project = typer.prompt("What's your Project Name?")
+              
     domain = typer.prompt("What's your registered Domain Name?")
+    
     auth_provider = typer.prompt("What's your Auth Provider?")
     namespace = typer.prompt("What's the Namespace?")
     repository = typer.prompt("What's your Repository Name?")
